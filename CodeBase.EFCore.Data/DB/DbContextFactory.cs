fd6ac8b7fa5fb44f85f9a362b5a73874f66f0855
DonPham <donpham@grapecity.com>
2024-12-30 13:47:13 +0700

    1st Migration base Db

---
+++ CodeBase.EFCore.Data/DB/DbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CodeBase.EFCore.Data.DB;
+
+public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+{
+    public DatabaseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+        optionsBuilder.UseSqlite("Data Source=../CodeBase.Utility/SQLLite/Codebase.db");
+        return new DatabaseContext(optionsBuilder.Options);
+    }
+}
